7c619f711ed6eb67c34d366d09e075a6a0119bbc
Alex Frost from RP4 <alexxjoker96@gmail.com>
2024-11-24 15:53:23 +0200

    Remove dump from get_nfdump.sh

--- get_nfdump.sh
+++ get_nfdump.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-#nfdump -R /var/log/netflow -s record/bytes -n 30 -A srcip,dstip  -t 2024/11/22.19:50:00 'net 192.168.50.0/24' -o csv > data.csv
-#nfdump -R /var/log/netflow -s record/bytes  -t 2024/11/22.00:00:00 -n 100 -A srcip,dstip  -o csv > data.csv
-#nfdump -R /var/log/netflow/10.1.1.1 -s record/bytes -n 100 -A srcip,dstip  'ip 192.168.50.22' -t 2024/11/23.06:00:00  -o csv > data.csv
-#nfdump -R /var/log/netflow -s record/bytes -n 10000000 -A srcip,dstip -o csv > data.csv
-#nfdump -R /var/log/netflow/ -s record/bytes -n 1000000 'port 80' -A srcip,dstip -o csv > data.csv
-#nfdump -R /var/log/netflow -s record/bytes -n 100 -A srcip,dstip -t 2024/11/24.07:00:00 'ip 192.168.50.11' -o csv > data.csv
 show_top() {
     local count=${1:-50}
     local time=${2:-false}
